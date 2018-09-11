22d5c5822261f8dabdc37e5f39c1710035b309e8
Mamie Wang <szmamie@pennmedicine.upenn.edu>
2018-09-11 11:36:25 -0400

    Find overlapping genes between two sets of experiments

--- R/gene_signature.R
+++ R/gene_signature.R
@@ -9,7 +9,7 @@ suppressMessages({
 #' @param child_node_1_idx a m by 1 logical vector that indicates where the cells for children node 1 are
 #' @param child_node_2_idx a m by 1 logical vector that indicates where the cells for children node 2 are
 #' @param q_thresh a positive real number that gives cutoff of q value of DE analysis
-#' @FC_thresh a positive real number that defines the cutoff of fold change the DE analysis
+#' @param FC_thresh a positive real number that defines the cutoff of fold change the DE analysis
 #' 
 #' @return a k by 2 matrix that contains k differentially expressed genes between the two child nodes of corresponding q value and FC value with rownames of the genes
 #' Usage:
@@ -33,4 +33,25 @@ DE_childrenNodes = function(X, child_node_1_idx, child_node_2_idx, q_thresh, FC_
     DE_hits = na.omit(DE_hits[order(DE_hits$log2FoldChange, decreasing=T),])
     DE_hits_final = DE_hits[DE_hits$log2FoldChange > log(FC_thresh, base=2),]
     return(data.matrix(DE_hits_final[,c(2, 6)]))
+}
+
+#' a wrapper function of DE_childrenNodes that finds the overlapping DE genes
+#'
+#' @param X_exp1, X_exp2 n genes by m cell count matrix of gene expression for two experiments
+#' @param cn_1_idx_exp1, cn_2_idx_exp2 a m by 1 logical vector that indicates where the cells for children node 1 are for experiement 1 and 2
+#' @param cn_2_idx_exp1, cn_2_idx_exp2 a m by 1 logical vector that indicates where the cells for children node 2 are for experiment 1 and 2
+#' @param q_thresh_1, q_thresh_2 a positive real number that gives cutoff of q value of DE analysis for experiment 1 and 2
+#' @param FC_thresh_1, FC_thresh_2 a positive real number that defines the cutoff of fold change the DE analysis for experiment 1 and 2
+#'
+#' @return the overlapping differentially expressed genes between experiment 1 and 2
+
+DE_twoExps = function(X_exp1, cn_1_idx_exp1, cn_2_idx_exp1, q_thresh_exp1, FC_thresh_exp1,
+                      X_exp2, cn_1_idx_exp2, cn_2_idx_exp2, q_thresh_exp2, FC_thresh_exp2) {
+       DE_hits_exp1 = DE_childrenNodes(X_exp1, cn_1_idx_exp1, cn_2_idx_exp1, q_thresh_exp1, FC_thresh_exp1)
+       DE_hits_exp2 = DE_childrenNodes(X_exp2, cn_1_idx_exp2, cn_2_idx_exp2, q_thresh_exp2, FC_thresh_exp2)
+       colnames(DE_hits_exp1) = c('log2FoldChange_exp1', 'padj_exp1')
+       colnames(DE_hits_exp2) = c('log2FoldChange_exp2', 'padj_exp2')
+       overlap_hits = intersect(rownames(DE_hits_exp1), rownames(DE_hits_exp2))
+       DE_hits_overlap = cbind(DE_hits_exp1[overlap_hits,], DE_hits_exp2[overlap_hits,])
+       return(DE_hits_overlap)
 }
