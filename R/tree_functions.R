e5beb1942c47e9b49d1e5efed9b1ddcc8210b8a6
Mamie Wang <szmamie@pennmedicine.upenn.edu>
2019-01-01 14:09:31 +0800

    Implemented align object and T matrix filling

--- R/tree_functions.R
+++ R/tree_functions.R
@@ -5,10 +5,13 @@
 #' @param right The right tree node
 #' @return A list representing the tree
 #' @export
-binary_tree_node <- function(label, left = NULL, right = NULL) {
-  node <- list(label = label, left = left, right = right)
-  return(node)
+binary_tree <- function(label, left = NULL, right = NULL) {
+  tree <- list(label = label, left = left, right = right)
+  class(tree) <- 'binary_tree'
+  return(tree)
 }
+
+make_children_map <- function()
 
 #' get the children labels
 #' 
@@ -21,59 +24,167 @@ get_children <- function(node) {
   return(children)
 }
 
+#' A skeleton function
+postorder <- function(x, f) {
+  if (!is.null(x$left)) postorder(x$left, f)
+  if (!is.null(x$right)) postorder(x$right, f)
+  f(x)
+}
+
 #' Get labels by postorder traversal
 #' 
 #' @param x A root node of tree
 #' @return labels of tree in postorder
 #' @export
-postorder <- function(x) {
-  label_ordered <- c()
-  if (!is.null(x$left)) label_ordered <- c(label_ordered, postorder(x$left))
-  if (!is.null(x$right)) label_ordered <- c(label_ordered, postorder(x$right))
-  label_ordered <- c(label_ordered, x$label)
-  return(label_ordered)
+postorder_labels <- function(x) {
+  labels <- c()
+  postorder(x, function(t) labels <<- c(labels, t$label))
+  return(labels)
 }
 
-#' Initialize the matrix to store alignment score
+#' Create alignment object
 #' @param T1 tree 1
 #' @param T2 tree 2
 #' @param cost_matrix The cost matrix
-#' @return A list of T matrix and F map
+#' @return An alignment object
 #' @export
-initialize <- function(T1, T2, cost_matrix) {
-
-  initialize_T1 <- function(x) {
-    if (!is.null(x$left)) {
-      initialize_T1(x$left)
-    }
-    if (!is.null(x$right)) {
-      initialize_T1(x$right)
-    }
-    children <- get_children(x)
-    F_map[[paste0('1_', x$label)]] <<- sum(T_matrix[children, "lambda"])
-    T_matrix[x$label, "lambda"] <<- F_map[[paste0('1_', x$label)]] + cost_matrix[x$label, "lambda"]
-  }
-
-  initialize_T2 <- function(x) {
-    if (!is.null(x$left)) {
-      initialize_T2(x$left)
-    }
-    if (!is.null(x$right)) {
-      initialize_T2(x$right)
-    }
-    children <- get_children(x)
-    F_map[[paste0('2_', x$label)]] <<- sum(T_matrix["lambda", children])
-    T_matrix["lambda", x$label] <<- F_map[[paste0('2_', x$label)]] + cost_matrix["lambda", x$label]
-  }
-  
-  ordered_T1 <- postorder(T1)
-  ordered_T2 <- postorder(T2)
+create_align_object <- function(T1, T2, cost_matrix) {
+  ordered_T1 <- postorder_labels(T1)
+  ordered_T2 <- postorder_labels(T2)
   T_matrix <- matrix(NA, nrow = length(ordered_T1) + 1, ncol = length(ordered_T2) + 1)
   rownames(T_matrix) <- c('lambda', ordered_T1)
   colnames(T_matrix) <- c('lambda', ordered_T2)
   T_matrix['lambda', 'lambda'] <- 0
   F_map <- hashmap::hashmap('lambda_lambda', 0)
-  initialize_T1(T1)
-  initialize_T2(T2)
-  return(list(T_matrix = T_matrix, F_map = F_map))
+  list(T1 = T1, T2 = T2, cost_matrix = cost_matrix, T_matrix = T_matrix, F_map = F_map)
+}
+
+initialize_T1 <- function(align_object) {
+  postorder(align_object$T1,
+            function(t) {
+              children <- get_children(t)
+              align_object$F_map[[paste_collapse(1, t$label, 1, 2, 'lambda')]] <<- sum(align_object$T_matrix[children, "lambda"])
+              align_object$T_matrix[t$label, "lambda"] <<- align_object$F_map[[paste_collapse(1, t$label, 1, 2, 'lambda')]] + align_object$cost_matrix[t$label, "lambda"]
+            })
+  return(align_object)
+}
+
+initialize_T2 <- function(align_object) {
+  postorder(align_object$T2,
+    function(t) {
+      children <- get_children(t)
+      align_object$F_map[[paste_collapse('lambda', 2, t$label, 1, 2)]] <<- sum(align_object$T_matrix["lambda", children])
+      align_object$T_matrix["lambda", t$label] <<- align_object$F_map[[paste_collapse('lambda', 2, t$label, 1, 2)]] + align_object$cost_matrix["lambda", t$label]
+    }
+  )
+  return(align_object)
+}
+
+initialize <- function(align_object) {
+  align_object <- initialize_T1(align_object)
+  align_object <- initialize_T2(align_object)
+  align_object$T1_children <- create_children_map(align_object$T1)
+  align_object$T2_children <- create_children_map(align_object$T2)
+  return(align_object)
+}
+
+create_children_map <- function(tree) {
+  childrens <- c()
+  labels <- c()
+  postorder(tree, function(x) {
+    left_children <- ifelse(is.null(x$left), 'lambda', x$left$label)
+    right_children <- ifelse(is.null(x$right), 'lambda', x$right$label)
+    childrens <<- rbind(childrens, c(left_children, right_children))
+    labels <<- c(labels, x$label)
+  })
+  rownames(childrens) <- labels
+  colnames(childrens) <- c('left', 'right')
+  return(childrens)
+}
+
+lambda_T2 <- function(align_obj, i, j) {
+  subtree_cost <- c()
+  T2_children_j <- align_obj$T2_children[j, ]
+  for (r in T2_children_j) {
+    subtree_cost <- c(subtree_cost, align_obj$T_matrix[i, r] - align_obj$T_matrix['lambda', r])
+  }
+  align_obj$T_matrix['lambda', j] + min(subtree_cost)
+}
+
+T1_lambda <- function(align_obj, i, j) {
+  subtree_cost <- c()
+  T1_children_i <- align_obj$T1_children[i, ]
+  for (r in T1_children_i) {
+    subtree_cost <- c(subtree_cost, align_obj$T_matrix[r, j] - align_obj$T_matrix[r, 'lambda'])
+  }
+  align_obj$T_matrix[i, 'lambda'] + min(subtree_cost)
+}
+
+T1_T2 <- function(align_obj, i, j) {
+  align_obj$F_map[[paste(1, i, 1, 2, 2, j, 1, 2, collapse = '_')]] + align_obj$cost_matrix[i, j]
+}
+
+paste_collapse <- function(...) paste0(..., collapse = '_')
+
+fill_matrix <- function(align_obj) {
+  align_obj$choice <- align_obj$T_matrix
+  align_obj$choice[,] <- NA
+  for (i in rownames(align_obj$T_matrix)[-1]) {
+    cost <- c()
+    for (j in colnames(T_matrix)[-1]) {
+      
+      for (s in seq(2)) {
+        align_obj <- fill_F(align_obj, paste_collapse(1, i, s, 2), 
+                            paste_collapse(2, j, 1, 2))
+      }
+      
+      for (t in seq(2)) {
+        align_obj <- fill_F(align_obj, paste_collapse(1, i, 1, 2), 
+                            paste_collapse(1, j, t, 2))
+      }
+      
+      cost <- c(cost, lambda_T2(align_obj, i, j))
+      cost <- c(cost, T1_lambda(align_obj, i, j))
+      cost <- c(cost, T1_T2(align_obj, i, j))
+      align_obj$choice[i, j] <- which.min(cost)
+      align_obj$T_matrix[i, j] <- min(cost)
+    }
+  }
+  return(align_obj)
+}
+
+fill_F <- function(align_obj, x, y) {
+  x <- strsplit(x, split = '_')[[1]][-1]
+  i <- x[1]
+  s <- as.numeric(x[2])
+  mi <- as.numeric(x[3])
+  y <- strsplit(y, split = '_')[[1]][-1]
+  j <- y[1]
+  t <- as.numeric(y[2])
+  nj <- as.numeric(y[3])
+  align_obj$F_map[[paste_collapse(1, i, s, s-1, 2, j, t, t - 1)]] <- 0
+  for (p in seq(s, mi)) {
+    align_obj$F_map[[paste_collapse(1, i, s, p, 2, j, t, t - 1)]] <- 
+      align_obj$F_map[[paste_collapse(1, i, s, p - 1, 2, j, t, t - 1)]] +
+      align_obj$T_matrix[align_obj$T1_children[i, p], 'lambda']
+  }
+  
+  for (q in seq(t, nj)) {
+    align_obj$F_map[[paste_collapse(1, i, s, s - 1, 2, j, t, q)]] <- 
+      align_obj$F_map[[paste_collapse(1, i, s, s - 1, 2, j, t, q - 1)]] +
+      align_obj$T_matrix['lambda', align_obj$T2_children[j, q]]
+  }
+  
+  for (p in seq(s, mi)) {
+    for (q in seq(t, nj)) {
+      align_obj <- fill_F_helper(align_obj, i, s, p, j, t, q)
+    }
+  }
+  return(align_obj)
+}
+
+fill_F_helper <- function(align_obj, i, s, p, j, t, q) {
+  choix_1 <- align_obj$F_map[[i, 1, s]]
+  align_obj$F_map[[paste_collapse(1, i, s, p, 2, j, t, q)]] <- min(res)
+  return(align_obj)
 }
--- data-raw/tree-matching.R
+++ data-raw/tree-matching.R
@@ -1,11 +1,11 @@
 # implement Jiang et al (1994) tree traversal
-T_1 <- ReSET::binary_tree_node('a', 
-                  left = tree_node('e', left = tree_node('b'), right = tree_node('c')),
-                  right = tree_node('d'))
+T_1 <- ReSET::binary_tree('a', 
+                  left = ReSET::binary_tree('e', left = ReSET::binary_tree('b'), right = ReSET::binary_tree('c')),
+                  right = ReSET::binary_tree('d'))
 
-T_2 <- ReSET::binary_tree_node('a', 
-                 left = tree_node('b'),
-                 right = tree_node('f', left = tree_node('c'), right = tree_node('d')))
+T_2 <- ReSET::binary_tree('a', 
+                 left = ReSET::binary_tree('b'),
+                 right = ReSET::binary_tree('f', left = ReSET::binary_tree('c'), right = ReSET::binary_tree('d')))
 
 # Create the cost matrix
 ordered_T1 <- ReSET::postorder(T_1)
@@ -20,4 +20,7 @@ cost_matrix <- rbind(lambda = rep(0.5, ncol(cost_matrix)), cost_matrix)
 cost_matrix <- cbind(lambda = c(0, rep(0.5, nrow(cost_matrix)-1)), cost_matrix)
 cost_matrix
 
-initialized <- ReSET::initialize(T_1, T_2, cost_matrix)
+align_obj <- ReSET::create_align_object(T_1, T_2, cost_matrix)
+align_obj <- ReSET::initialize(align_obj)
+
+
